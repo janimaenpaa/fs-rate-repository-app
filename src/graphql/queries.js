5be5efcb8d6afa476a6ce0e2357e028e5f370a4e
janimaenpaa <itsmayd@gmail.com>
2021-02-11 16:43:06 +0200

    Done 10.11

---
+++ src/graphql/queries.js
@@ -0,0 +1,23 @@
+import { gql } from "apollo-boost";
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      edges {
+        node {
+          id
+          ownerName
+          name
+          fullName
+          ratingAverage
+          reviewCount
+          stargazersCount
+          forksCount
+          ownerAvatarUrl
+          description
+          language
+        }
+      }
+    }
+  }
+`;
