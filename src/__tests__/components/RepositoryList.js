caba01cc17b18d1027e2b20dc16939dceaa31926
janimaenpaa <itsmayd@gmail.com>
2021-02-13 16:13:53 +0200

    Remove unused import

--- src/__tests__/components/RepositoryList.js
+++ src/__tests__/components/RepositoryList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render } from "@testing-library/react-native";
 import { RepositoryListContainer } from "../../components/RepositoryList";
 
 /* eslint-disable jest/expect-expect */
