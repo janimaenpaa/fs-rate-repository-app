d27663133d74bfe8e3dde7968b914924aa8010f8
janimaenpaa <itsmayd@gmail.com>
2021-03-25 15:56:02 +0200

    Done 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, View, StyleSheet } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 
@@ -11,7 +12,11 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({
+  repositories,
+  sortBy,
+  setSortBy,
+}) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -21,14 +26,35 @@ export const RepositoryListContainer = ({ repositories }) => {
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem key={item.id} {...item} />}
+      ListHeaderComponent={() => (
+        <Picker
+          selectedValue={sortBy}
+          style={{ height: 50, margin: 10 }}
+          onValueChange={(itemValue) => setSortBy(itemValue)}
+        >
+          <Picker.Item label="Latest repositories" value="latest" />
+          <Picker.Item
+            label="Highest rated repositories"
+            value="highestRated"
+          />
+          <Picker.Item label="Lowest rated repositories" value="lowestRated" />
+        </Picker>
+      )}
     />
   );
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [sortBy, setSortBy] = useState("latest");
+  const { repositories } = useRepositories(sortBy);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      sortBy={sortBy}
+      setSortBy={setSortBy}
+    />
+  );
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,8 +1,15 @@
 import { useQuery } from "@apollo/react-hooks";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const sortOptions = {
+  latest: { orderBy: "CREATED_AT", orderDirection: "DESC" },
+  highestRated: { orderBy: "RATING_AVERAGE", orderDirection: "DESC" },
+  lowestRated: { orderBy: "RATING_AVERAGE", orderDirection: "ASC" },
+};
+
+const useRepositories = (sortBy) => {
   const { loading, data, refetch } = useQuery(GET_REPOSITORIES, {
+    variables: sortOptions[sortBy],
     fetchPolicy: "cache-and-network",
   });
 
